4f830e1371d36593cd37c9a5a5a102df57b44a9b
Brandon <bkreiser5@gmail.com>
2019-02-20 11:49:06 -0500

    09/21/18

---
+++ IllegalPositionException.java
@@ -0,0 +1,11 @@
+//Brandon Kreiser
+
+public class IllegalPositionException extends RuntimeException
+{
+    public IllegalPositionException() {}
+
+    public IllegalPositionException(String msg)
+    {
+        super(msg);
+    }
+}
---
+++ IllegalRemoveException.java
@@ -0,0 +1,11 @@
+//Brandon Kreiser
+
+public class IllegalRemoveException extends RuntimeException
+{
+    public IllegalRemoveException() {}
+
+    public IllegalRemoveException(String msg)
+    {
+        super(msg);
+    }
+}
